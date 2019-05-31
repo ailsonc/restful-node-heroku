f5f9f11fcef3328a88bf02fa3eaea4aedd7c5221
ailson <ailsonlcosta@gmail.com>
2019-05-30 23:31:36 -0300

    process.env.PORT

--- serve.js
+++ serve.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 routes(app);
 
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log('Servidor porta: '+ PORT);
 });
